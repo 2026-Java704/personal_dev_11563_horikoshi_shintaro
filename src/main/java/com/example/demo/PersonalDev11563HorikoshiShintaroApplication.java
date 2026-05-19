f2e78fd4603e2e97c97d38038e4f0449da8ae405
student <student@FJ-1025>
2026-05-19 11:26:39 +0900

    初回コミット

---
+++ src/main/java/com/example/demo/PersonalDev11563HorikoshiShintaroApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonalDev11563HorikoshiShintaroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonalDev11563HorikoshiShintaroApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/example/demo/model/Account.java
@@ -0,0 +1,69 @@
+package com.example.demo.model;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.context.annotation.SessionScope;
+
+@Component
+@SessionScope
+public class Account {
+
+	private Integer id;
+
+	private String name; // 名前
+
+	private String address; // 住所
+
+	private String tel; // 電話番号
+
+	private String email; // メールアドレス
+
+	private String password;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public String getTel() {
+		return tel;
+	}
+
+	public void setTel(String tel) {
+		this.tel = tel;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+}
