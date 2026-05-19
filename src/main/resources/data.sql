ad3ad1c6a12c6d6a58e3c95e3c954639cc17b60f
student <student@FJ-1025>
2026-05-19 16:35:14 +0900

    SQLに例文を追加

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -8,7 +8,13 @@ INSERT INTO genres(genre_name, is_income) VALUES('その他の支出', FALSE);
 -- ユーザテーブルデータ
 INSERT INTO users(user_name,email,password) VALUES('田中太郎', 'tanaka@aaa.com','himitu');
 INSERT INTO users(user_name,email,password) VALUES('鈴木一郎', 'suzuki@aaa.com','himitu');
+INSERT INTO users(user_name,email,password) VALUES('test', 'test','test');
+INSERT INTO users(user_name,email,password) VALUES('test2', 'test2','test2');
 
 -- 項目テーブルデータ
 INSERT INTO items(item_name, user_id, genre_id, price,add_date) VALUES('食事代', 1, 3, 1200, '2026/05/01');
 INSERT INTO items(item_name, user_id, genre_id, price,add_date) VALUES('給料', 2, 1, 200000, '2026/05/15');
+INSERT INTO items(item_name, user_id, genre_id, price,add_date) VALUES('食事代', 3, 3, 1200, '2026/05/01');
+INSERT INTO items(item_name, user_id, genre_id, price,add_date) VALUES('給料', 3, 1, 200000, '2026/05/15');
+INSERT INTO items(item_name, user_id, genre_id, price,add_date) VALUES('映画代', 4, 3, 1600, '2026/05/01');
+INSERT INTO items(item_name, user_id, genre_id, price,add_date) VALUES('お小遣い', 4, 4, 2000, '2026/05/15');
