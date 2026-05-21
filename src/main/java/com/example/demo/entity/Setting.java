52a3f2d144f3111a4424c71e0c1792473a3cb444
student <student@FJ-1025>
2026-05-21 15:24:52 +0900

    項目がポップアップで追加できるように修正

--- src/main/java/com/example/demo/entity/Setting.java
+++ src/main/java/com/example/demo/entity/Setting.java
@@ -1,5 +1,7 @@
 package com.example.demo.entity;
 
+import java.time.LocalDate;
+
 public class Setting {
 
 	/**全ての項目を表示するかどうか**/
@@ -8,9 +10,17 @@ public class Setting {
 	/**表示する項目ID**/
 	private Integer showGenreId = null;
 
-	public Setting(Boolean showAll, Integer showGenreId) {
+	/**表示する月**/
+	private LocalDate showingDate = null;
+
+	/**選択している日付**/
+	private LocalDate selectedDate = null;
+
+	public Setting(Boolean showAll, Integer showGenreId, LocalDate showingDate, LocalDate selectedDate) {
 		this.showAll = showAll;
 		this.showGenreId = showGenreId;
+		this.showingDate = showingDate;
+		this.selectedDate = selectedDate;
 	}
 
 	public Boolean isShowAll() {
@@ -27,6 +37,26 @@ public class Setting {
 
 	public void setShowGenreId(Integer showGenreId) {
 		this.showGenreId = showGenreId;
+	}
+
+	public LocalDate getShowingDate() {
+		return showingDate;
+	}
+
+	public void setShowingDate(LocalDate showingDate) {
+		this.showingDate = showingDate;
+	}
+
+	public LocalDate getSelectedDate() {
+		return selectedDate;
+	}
+
+	public void setSelectedDate(LocalDate selectedDate) {
+		this.selectedDate = selectedDate;
+	}
+
+	public Boolean getShowAll() {
+		return showAll;
 	}
 
 }
